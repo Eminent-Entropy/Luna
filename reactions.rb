7bc45c149231c65554b071c03df8a2d9a7432c40
Aiden G <orca950@gmail.com>
2020-11-17 23:25:37 -0600

    more updates

--- reactions.rb
+++ reactions.rb
@@ -1,23 +1,65 @@
 require 'discordrb'
 
 #ID
-TestingServer = 346043227364655127
-Thing1 = 417687051735924736
+SERVER = 566092415690211349
+RAINBOWSIX = 778465651052249099
+MINECRAFT = 778466125859258409
+TERRAIRA = 778466282767908907
+DCS = 778465831721369630
+ELITE = 778465709788626966
+NMS = 778466394643365929
+STELLARIS = 778466465543094293
 
 def reaction_add(bot, event)
   user = event.user
-  thing1 = bot.server(TestingServer).role(Thing1)
+  rainbowsix = bot.server(SERVER).role(RAINBOWSIX)
+  mincraft = bot.server(SERVER).role(MINECRAFT)
+  terraira = bot.server(SERVER).role(TERRAIRA)
+  dcs = bot.server(SERVER).role(DCS)
+  elite = bot.server(SERVER).role(ELITE)
+  nms = bot.server(SERVER).role(NMS)
+  stellaris = bot.server(SERVER).role(STELLARIS)
 
-  if event.emoji.name == 'oops'
-    user.add_role(thing1)
+  if event.emoji.name == 'rainbow6'
+    user.add_role(rainbowsix)
+  elsif event.emoji.name == 'minecraft'
+    user.add_role(mincraft)
+  elsif event.emoji.name == 'terraria'
+    user.add_role(terraira)
+  elsif event.emoji.name == 'dcs'
+    user.add_role(dcs)
+  elsif event.emoji.name == 'elite'
+    user.add_role(elite)
+  elsif event.emoji.name == 'nms'
+    user.add_role(nms)
+  elsif event.emoji.name == 'stellaris'
+    user.add_role(stellaris)
   end
 end
 
 def reaction_remove(bot, event)
   user = event.user
-  thing1 = bot.server(TestingServer).role(Thing1)
+  rainbowsix = bot.server(SERVER).role(RAINBOWSIX)
+  mincraft = bot.server(SERVER).role(MINECRAFT)
+  terraira = bot.server(SERVER).role(TERRAIRA)
+  dcs = bot.server(SERVER).role(DCS)
+  elite = bot.server(SERVER).role(ELITE)
+  nms = bot.server(SERVER).role(NMS)
+  stellaris = bot.server(SERVER).role(STELLARIS)
 
-  if event.emoji.name == 'oops'
-    user.remove_role(thing1)
+  if event.emoji.name == 'rainbow6'
+    user.remove_role(rainbowsix)
+  elsif event.emoji.name == 'minecraft'
+    user.remove_role(mincraft)
+  elsif event.emoji.name == 'terraria'
+    user.remove_role(terraira)
+  elsif event.emoji.name == 'dcs'
+    user.remove_role(dcs)
+  elsif event.emoji.name == 'elite'
+    user.remove_role(elite)
+  elsif event.emoji.name == 'nms'
+    user.remove_role(nms)
+  elsif event.emoji.name == 'stellaris'
+    user.remove_role(stellaris)
   end
 end
